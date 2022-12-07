39eca2d81741a125bc2c3487c41354fd91beadb9
Ajay957 <ajay74.aa82@gmail.com>
2022-12-07 14:38:45 +0530

    search added

--- src/main/java/com/example/employee_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/employee_backend/dao/EmployeeDao.java
@@ -1,7 +1,13 @@
 package com.example.employee_backend.dao;
 
 import com.example.employee_backend.model.Employees;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employees,Integer> {
+    @Query(value = "SELECT `id`, `com_name`, `designation`, `emp_code`, `emp_name`, `mob_no`, `password`, `salary`, `username` FROM `employees` WHERE `emp_code`= :empcode",nativeQuery = true)
+    List<Employees> SearchEmployee(@Param("empcode") Integer empcode);
 }
